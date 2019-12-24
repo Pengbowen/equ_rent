4794e256dcb85f2f975d0908a897769fc2c43c7f
pbw <326301416@qq.com>
2019-12-24 21:51:38 +0800

    1，修复bug

--- src/main/java/com/bob/equrent/service/UserService.java
+++ src/main/java/com/bob/equrent/service/UserService.java
@@ -1,23 +1,17 @@
 package com.bob.equrent.service;
 
-import cn.hutool.core.util.PageUtil;
 import cn.hutool.core.util.StrUtil;
 import com.bob.equrent.dao.UserDao;
-import com.bob.equrent.entity.ResponsePageData;
 import com.bob.equrent.entity.User;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Service
@@ -27,23 +21,17 @@ public class UserService {
     private UserDao userDao;
 
 
-    public  Page<User> findUserPage(User user,int pageSize, int pageCount,){
+    public  Page<User> findUserPage(User user,int pageSize, int pageCount){
         List<Predicate> predicates = Lists.newArrayList();
-        Specification<User> specification = new Specification<User>(){
-            @Override
-            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                if (StrUtil.isNotEmpty(user.getName()))  predicates.add(criteriaBuilder.like(root.get("name"),"%"+user.getName()+"%"));
-                if (StrUtil.isNotEmpty(user.getNikcname()))  predicates.add(criteriaBuilder.like(root.get("id"),"%"+user.getNikcname()+"%"));
-                if (StrUtil.isNotEmpty(user.getMobile()))  predicates.add(criteriaBuilder.like(root.get("id"),"%"+user.getMobile()+"%"));
-                if (StrUtil.isNotEmpty(user.getEmail()))  predicates.add(criteriaBuilder.like(root.get("id"),"%"+user.getEmail()+"%"));
-                return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
-            }
+        Specification<User> specification = (Specification<User>) (root, criteriaQuery, criteriaBuilder) -> {
+            if (StrUtil.isNotEmpty(user.getName()))  predicates.add(criteriaBuilder.like(root.get("name"),"%"+user.getName()+"%"));
+            if (StrUtil.isNotEmpty(user.getNikcname()))  predicates.add(criteriaBuilder.like(root.get("nickname"),"%"+user.getNikcname()+"%"));
+            if (StrUtil.isNotEmpty(user.getMobile()))  predicates.add(criteriaBuilder.like(root.get("mobile"),"%"+user.getMobile()+"%"));
+            if (StrUtil.isNotEmpty(user.getEmail()))  predicates.add(criteriaBuilder.like(root.get("email"),"%"+user.getEmail()+"%"));
+            if (user.getId() != null)  predicates.add(criteriaBuilder.equal(root.get("id"),+user.getId()));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
         };
         Pageable pageable = PageRequest.of(pageCount,pageSize);
         return this.userDao.findAll(specification,pageable);
-    }
-
-    public  Page<User> findUserPage(User user){
-
     }
 }
